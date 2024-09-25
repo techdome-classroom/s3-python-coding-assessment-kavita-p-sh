1cdd06fafc2260b99e3fef7885a2ca73b9c4a3d7
Kavita Prajapati <prajapatikavita132004@gmail.com>
2024-09-25 16:38:05 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,35 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-
-
         bracket_map = {')': '(', '}': '{', ']': '['}
-       
         stack = []
 
-
         for char in s:
-
             if char in bracket_map:
-
                 top_element = stack.pop() if stack else '#'
-
                 if bracket_map[char] != top_element:
                     return False
             else:
-
                 stack.append(char)
         
-
         return not stack
-
-        pass
-
-
-
-
-
-
 
     
 
