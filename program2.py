7fe99e95eef3012248ced81792f7c30f167b1da8
Kavita Prajapati <prajapatikavita132004@gmail.com>
2024-09-25 15:59:28 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -17,7 +17,7 @@ class Solution(object):
             # Get the integer value of the current Roman numeral
             current_value = roman_map[char]
             
-            # If the current value is smaller than the previous one, subtract it (e.g., IV, IX)
+
             if current_value < prev_value:
                 total -= current_value
             else:
