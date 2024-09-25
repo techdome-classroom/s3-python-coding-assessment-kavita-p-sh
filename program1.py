07358664a0a9380ca881fbef17f5b1a9ce1f9606
Kavita Prajapati <prajapatikavita132004@gmail.com>
2024-09-25 16:00:33 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -16,7 +16,7 @@ class Solution(object):
             if char in bracket_map:
 
                 top_element = stack.pop() if stack else '#'
-                # Check if the top element matches the corresponding opening bracket
+
                 if bracket_map[char] != top_element:
                     return False
             else:
