8bdf38ebf83c518769ee2e779fad4c96765521a1
Kavita Prajapati <prajapatikavita132004@gmail.com>
2024-09-25 15:57:28 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -5,12 +5,7 @@ class Solution(object):
         :rtype: int
         """
 
-        class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+        
         # Roman numeral to integer mapping
         roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         
